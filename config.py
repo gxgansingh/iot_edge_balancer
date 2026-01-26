1975b1e9b5a3c9994bbe238b52e803687eb42a5d
gxgansingh <ryangagan123@gmail.com>
2026-01-27 01:21:05 +0530

    increased speed

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 # ----------------- IOT SENSOR SETTINGS -----------------
-NUM_SENSORS = 5                     # Number of virtual sensors to simulate
-GENERATION_INTERVAL = 1.0           # Frequency of data generation in seconds
+NUM_SENSORS = 5                     
+GENERATION_INTERVAL = 0.1           # CHANGE: Fast Data! (Pehle 1.0 tha, ab 0.1 kiya)
 
 # ----------------- BALANCER SETTINGS -------------------
-CPU_THRESHOLD = 75.0                # Threshold percentage to classify a core as 'overloaded'
-WORKER_PROCESSES = os.cpu_count()   # Dynamically counts the total CPU cores on the host machine
+CPU_THRESHOLD = 75.0                
+WORKER_PROCESSES = os.cpu_count()   
 
 # ----------------- LOGGING SETTINGS --------------------
-LOG_FILE_PATH = "logs/system_metrics.csv" # Path to store performance metrics
+LOG_FILE_PATH = "logs/system_metrics.csv" 
 
 print(f"System Configuration Loaded: Utilizing {WORKER_PROCESSES} CPU cores for load balancing.")
